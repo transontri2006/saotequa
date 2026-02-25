b830531edd5895d34d9398a4e39d487d9d3b7930
transontri2006 <transontri2006.fpt@gmail.com>
2026-02-25 22:09:48 +0700

    Update MeetingRoom.java

--- O_Room/MeetingRoom.java
+++ O_Room/MeetingRoom.java
@@ -1,43 +1,15 @@
-package O_Room;
-
+package room;
 import java.util.Scanner;
 
 public class MeetingRoom extends Room {
-
     private int capacity;
-    private Scanner sc = new Scanner(System.in);
-
-    public MeetingRoom() {
-        super();   // gọi constructor rỗng của Room
-    }
-
-    public MeetingRoom(String id, String name, double baseCost, int capacity) {
-        super(id, name, baseCost);  // gọi constructor 3 tham số của Room
-        this.capacity = capacity;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
     @Override
     public void addRoom() {
-        super.addRoom();   // nhập id, name, baseCost
-        System.out.print("Enter capacity: ");
+        Scanner sc = new Scanner(System.in);
+        super.addRoom();
+        System.out.print("Capacity: ");
         capacity = sc.nextInt();
-        sc.nextLine();     // clear buffer
-    }
-
-    @Override
-    public void updateRoom() {
-        super.updateRoom();
-        System.out.print("Update capacity: ");
-        capacity = sc.nextInt();
-        sc.nextLine();
     }
 
     @Override
