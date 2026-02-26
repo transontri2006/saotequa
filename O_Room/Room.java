39e1065fa7ba22e6d7307784580fdb208b8e177c
transontri2006 <transontri2006.fpt@gmail.com>
2026-02-26 20:57:58 +0700

    Update Room.java

--- O_Room/Room.java
+++ O_Room/Room.java
@@ -1,50 +1,77 @@
 package O_Room;
+
 import java.util.Scanner;
 
 public abstract class Room implements IRoom {
-    private String id; 
-    private String name; 
+    private String id;
+    private String name;
     private double baseCost;
-    protected Scanner sc = new Scanner(System.in); 
+    protected Scanner sc = new Scanner(System.in);
 
-    public Room() {} 
+    public Room() {
+    }
 
     public Room(String id, String name, double baseCost) {
         this.id = id;
         this.name = name;
         this.baseCost = baseCost;
+    }
+
 
     public String getId() {
-    public void setId(String id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public double getBaseCost() { return baseCost; }
-    public void setBaseCost(double baseCost) { this.baseCost = baseCost; }
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBaseCost() {
+        return baseCost;
+    }
+
+    public void setBaseCost(double baseCost) {
+        this.baseCost = baseCost;
+    }
 
     @Override
     public void addRoom() {
-        System.out.print("Enter ID: ");
-        setId(sc.nextLine());
-        System.out.print("Enter Name: ");
-        setName(sc.nextLine());
-        System.out.print("Enter Base Cost: ");
-        setBaseCost(sc.nextDouble());
-        sc.nextLine(); // Consume newline
+        System.out.print("Input ID: ");
+        id = sc.nextLine();
+
+        System.out.print("Input Name: ");
+        name = sc.nextLine();
+
+        System.out.print("Input Base Cost: ");
+        baseCost = sc.nextDouble();
+        sc.nextLine();
     }
 
     @Override
     public void updateRoom() {
-        System.out.print("Enter New Name: ");
-        setName(sc.nextLine()); 
-        System.out.print("Enter New Base Cost: ");
-        setBaseCost(sc.nextDouble()); 
-        sc.nextLine(); // Consume newline
+        System.out.print("New Name: ");
+        name = sc.nextLine();
+
+        System.out.print("New Base Cost: ");
+        baseCost = sc.nextDouble();
+        sc.nextLine();
     }
 
     @Override
     public void displayDetails() {
-        System.out.print("ID: " + getId() + ", Name: " + getName() + ", Base Cost: " + getBaseCost());
+        System.out.print("ID: " + id +
+                         ", Name: " + name +
+                         ", Base Cost: " + baseCost);
     }
 }
+
 
 
